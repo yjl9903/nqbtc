40780e87bbc758a868d8a409974237353799eab9
yjl9903 <yjl9903@onekuma.cn>
2026-02-17 17:28:10 +0800

    chore: update package.json

--- packages/nqbtc/src/cli.ts
+++ packages/nqbtc/src/cli.ts
@@ -1,9 +1,14 @@
 import { breadc } from 'breadc';
 
-import { version } from '../package.json';
+import { version, description } from '../package.json';
 
-const cli = breadc('nqbtc', { version });
+const cli = breadc('nqbtc', { version, description });
 
-cli.command('mcp', 'Start mcp server').action(async () => {});
+cli
+  .command('mcp', 'Start mcp server')
+  .option('--base-url <string>', 'qBittorrent WebUI API base URL')
+  .option('--username <string>', 'qBittorrent WebUI API auth username')
+  .option('--password <string>', 'qBittorrent WebUI API auth password')
+  .action(async () => {});
 
 await cli.run(process.argv.slice(2)).catch((err) => console.error(err));
